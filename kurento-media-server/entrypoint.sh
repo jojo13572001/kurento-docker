ec75102ea33778d7f258c65a64ffbbf23c77d94d
bean <jojo13572001@yahoo.com.tw>
2018-02-22 19:48:59 +0800

    change port to 30063

--- kurento-media-server/entrypoint.sh
+++ kurento-media-server/entrypoint.sh
@@ -14,8 +14,8 @@ fi
 # Remove ipv6 local loop until ipv6 is supported
 cat /etc/hosts | sed '/::1/d' | tee /etc/hosts > /dev/null
 
+sed -i 's/8888/30063/g' /etc/kurento/kurento.conf.json
 exec /usr/bin/kurento-media-server "$@" &
-sleep 2
 git clone -b streetlight https://github.com/jojo13572001/kurento-IPCam-H264.git
 cd kurento-IPCam-H264 && bower install --allow-root
 exec http-server -p 8443
